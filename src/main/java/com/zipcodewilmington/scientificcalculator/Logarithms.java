23a6d9a6bf0ec05a45ae764369b381f0bc729872
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-26 18:50:42 -0500

    Finished Logarithms Class

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Logarithms.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class Log {
+    public static double Log (double number){
+        return Math.log(number);
+    }
+    public static double inverseLog(double x){
+        return Math.pow(10, x);
+    }
+    Ln (natural logarithm)
+    ex (inverse natural logarithm)
+}
