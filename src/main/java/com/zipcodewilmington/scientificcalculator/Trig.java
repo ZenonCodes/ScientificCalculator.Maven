8b399986d0eb24a3b6339c8f5284a90a814b7dff
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-26 14:45:39 -0500

    Redid naming according to ReadMe

--- src/main/java/com/zipcodewilmington/scientificcalculator/Trig.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Trig.java
@@ -2,33 +2,33 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class Trig {
     // Takes radian x and returns sin(x)
-    public static double calcSin(double angle) {
+    public static double sine(double angle) {
         return Math.sin(angle);
     }
     // Takes radian x and returns cos(x)
-    public static double calcCos(double angle) {
+    public static double cosine(double angle) {
         return Math.cos(angle);
     }
     // Takes radian x and returns tan(x)
-    public static double calcTan(double angle) {
+    public static double tangent(double angle) {
         return Math.tan(angle);
     }
 
     // Takes params opp and hyp and divides
     // to get ratio and returns inverse sin
-    public static double invSin(double opp, double hyp) {
+    public static double inverseSine(double opp, double hyp) {
         double sinRatio = opp/hyp;
         return Math.asin(sinRatio);
     }
     // Takes params adj and hyp and divides
     // to get ratio and returns inverse cos
-    public static double invCos(double adj, double hyp) {
+    public static double inverseCosine(double adj, double hyp) {
         double cosRatio = adj/hyp;
         return Math.acos(cosRatio);
     }
     // Takes params opp and adj and divides
     // to get ratio and returns inverse tan
-    public static double invTan(double opp, double adj) {
+    public static double inverseTangent(double opp, double adj) {
         double tanRatio = opp/adj;
         return Math.atan(tanRatio);
     }
