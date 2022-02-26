22e1e8d00c272164fc24d43a827a129b280bb061
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-26 14:58:38 -0500

    Working on switchDisplayMode rotate options

--- src/main/java/com/zipcodewilmington/scientificcalculator/DisplayMode.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/DisplayMode.java
@@ -1,2 +1,34 @@
-package com.zipcodewilmington.scientificcalculator;public class DisplayMode {
+package com.zipcodewilmington.scientificcalculator;
+
+public class DisplayMode {
+    static boolean binary = false;
+    static boolean octal = false;
+    static boolean decimal = false;
+    static boolean hexadecimal = false;
+
+    public static String switchDisplayMode(){
+        String displayMode = null;
+        if (!binary && !octal && !decimal && !hexadecimal){
+            displayMode = "binary";
+            binary = true;
+        } else if (binary == true) {
+            displayMode = "octal";
+            binary = false;
+            octal = true;
+        } else if (octal == true) {
+            displayMode = "decimal";
+            octal = false;
+            decimal = true;
+        } else if (decimal == true) {
+            displayMode = "hexadecimal";
+            decimal = false;
+            hexadecimal = true;
+        }
+        return displayMode;
+    }
+
+
+    public static void switchDisplayMode(String mode){
+
+    }
 }
