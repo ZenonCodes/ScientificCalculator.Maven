d0946a72d35571645e90d3652f83d012ff2e49be
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-26 10:46:15 -0500

    Added sin function to MainAPP

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,6 +4,20 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+    /**
+     * calcSin method takes two doubles opposite and hypotenuse.
+     *  It divides opposite by hypotenuse and returns the double sin.
+     *  If hyp = 0, it returns "impossible"
+     *  Reference:https://www.w3schools.com/java/java_try_catch.asp
+     */
+    static double calcSin(double opp, double hyp){
+        if (hyp == 0) {
+            throw new ArithmeticException("Impossible! You can't divide by zero.");
+        }
+
+        double sin = opp/hyp;
+        return sin;
+    }
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -4,4 +4,7 @@ package com.zipcodewilmington.scientific_calculator;
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    public static double calcSin(double opposite, double hypotenuse) {
+        return 0;
+    }
 }
