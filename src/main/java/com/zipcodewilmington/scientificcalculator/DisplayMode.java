b641045db4df9b426ea30d2605e96389deae5f49
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-27 12:24:24 -0500

    Adding method comment explanations for DisplayMode

--- src/main/java/com/zipcodewilmington/scientificcalculator/DisplayMode.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/DisplayMode.java
@@ -7,6 +7,9 @@ public class DisplayMode {
     static boolean hexadecimal = false;
     static String displayMode = "decimal";
 
+    /**
+     * Cycles through display modes and updates the corresponding fields.
+     */
     public static void switchDisplayMode(){
         if (!binary && !octal && !decimal && !hexadecimal){
             displayMode = "binary";
@@ -31,7 +34,10 @@ public class DisplayMode {
 
     }
 
-
+    /**
+     * @param mode --> Accepts String as a parameter and updates the corresponding DisplayMode fields.
+     * The selection alternative to cycling through display modes.
+     */
     public static void switchDisplayMode(String mode){
         displayMode = mode.toLowerCase();
         if(displayMode.equals("binary")){
