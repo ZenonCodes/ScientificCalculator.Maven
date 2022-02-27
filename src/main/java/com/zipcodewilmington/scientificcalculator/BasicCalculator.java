59c53fcf90082d791214f6bea9abb782312ff924
Vandana <vandana@Zipcoders-MBP.fios-router.home>
2022-02-27 11:38:25 -0500

    refined the code into 2 methods,  for  double inputs and String input

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicCalculator.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BasicCalculator {
     double num1 = 0.0;
     double num2 = 0.0;
-    static char op;
+    static String op;
     static double result = 0.0; //current  state displayed on calculator, call this variable before every test
     static Scanner scan;
 
@@ -21,77 +21,76 @@ public class BasicCalculator {
 */
 
     /**
-     * @param op   : used for operations like +,-,* and /
+     * @param op   : used for operations like +,-,*,/, E->Exponential, IS ->Inverse Sign
+     *               SQR -> SquareRoot, SQ -->Square
      * @param num1 : input number1
      * @param num2 : input number 2
      * @return : Returns the result of all 4 operations depending on the 'op' is passed
      */
-    public double basicOperations ( char op, double num1, double num2 ) {
-//Similarly this to prompt user input
-//       Scanner scan=new Scanner(System.in);
-//      System.out.println("Please enter number1");
-//         num1= scan.nextDouble();
-//      System.out.println("Please enter number2");
-//       num2=scan.nextDouble();
-        if (op == '+') {
+    public double basicOperations ( String op, double num1, double num2 ) {
+
+        if (op.equals( "+")) {
+
             result = num1 + num2;
-        } else if (op == '-') {
+        } else if (op.equals( "-")) {
             result = num1 - num2;
-        } else if (op == '*') {
+        } else if (op.equals( "*")) {
             double mul = num1 * num2;
             result = Double.parseDouble(String.format("%.2f", mul));
-        } else if (op == '/') {
+        } else if (op.equals( "/")){
              if(num2!=0) {
                  double div = num1 / num2;
                  result = Double.parseDouble(String.format("%.2f", div));
              }
                else{
+                   String str="Infinity";
+                   result = Double.parseDouble(str);
                  System.out.println("Divide by zero error encountered");
              }
-        } else {
+
+        }
+        else if(op.equalsIgnoreCase("E")){
+            result=Math.pow(num1,num2);
+
+
+        }
+        else if(op.equalsIgnoreCase("IS")){
+            result=num1*-1;
+        }
+
+        else if(op.equalsIgnoreCase("SQR")){
+            result=Math.sqrt(num1);
+
+
+        }
+        else if(op.equalsIgnoreCase("SQ")){
+            result=num1*num1;
+
+
+        }
+        else {
             System.out.println("please enter proper operation");
         }
         return result;
     }
 
     /**
-     * @param x --> This Parameter is input for the square
-     * @return --> square of the input number
+     * @param number--> Accepts double  input   parameter
+     * @return -> return the inverse of  number  parameter 1/number
      */
-    public double getSquare ( double x ) {
-        return (x * x);
-    }
+    public String getInverseOfNumber ( double  number) {
+        String  res = "";
+        if (number==0|| number ==1) {
+            res = String.valueOf(number);
 
-    /**
-     * @param x --> This parameter is input for the squareroot
-     * @return -> Returns the squareroot of the input provided
-     */
-    public double getSquareRoot ( double x ) {
-        return Math.sqrt(x);
-    }
-
-    /**
-     * @param x ->This Parameter is base
-     * @param y --> This Parameter is exponent
-     * @return --> returns x^y;
-     */
-    public double getExponentiation ( double x, double y ) {
-        return Math.pow(x, y);
-    }
-
-    /**
-     * @param x --> Input parameter
-     * @return -> return the inverse of  input parameter
-     */
-    public double getInverseOfNumber ( double x ) {
-        double res = 0.0;
-
-        if (x!=0) {
-            res = (1 / x);
         } else {
-            System.out.println("Divide by Zero error encountered");
+            res="1/"+String.valueOf(number);
 
         }
         return res;
     }
+
+
+
+
 }
--- src/test/java/com/zipcodewilmington/scientificcalculator/BasicCalculatorTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/BasicCalculatorTest.java
@@ -5,24 +5,39 @@ package com.zipcodewilmington.scientificcalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 
 public class BasicCalculatorTest {
+
+    BasicCalculator test=new BasicCalculator();
+
          @Test
         public void addTest(){
+             double expected=6; //addition
 
-            BasicCalculator test=new BasicCalculator();
-            double expected=3; //addition
-
-            double  actual =test.basicOperations('/', 3,1);
+            double  actual =test.basicOperations("+", 3,3);
              System.out.println("The actual value:"+actual);
 
              Assertions.assertEquals(expected,actual);
          }
 
-         @Test
-    public void testSqrt(){
 
+         @Test
+    public void sqrtTest(){
+             double expected=9;
+             double actual=test.basicOperations("SQ",3,0);
+             System.out.println(actual);
+             Assertions.assertEquals(expected,actual);
+
+         }
+
+         @Test
+    public void inverseNumberTest(){
+             String  expected= "1/5.0";
+             String actual=test.getInverseOfNumber(5);
+             System.out.println("Actual value:"+actual);
+             Assertions.assertEquals(expected,actual);
          }
 }
