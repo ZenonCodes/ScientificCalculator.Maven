c4d07cb26c73b347098994302db1f2157007e50b
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-27 12:35:47 -0500

    Adding method comment explanations for TrigUnits

--- src/main/java/com/zipcodewilmington/scientificcalculator/TrigUnits.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/TrigUnits.java
@@ -4,6 +4,10 @@ public class TrigUnits {
     static boolean radians = true;
     static boolean degrees = false;
 
+    /**
+     * Cycles through trig units and updates corresponding fields.
+     */
+
     public static void switchUnitsMode(){
         if (radians) {
             radians = false;
@@ -13,6 +17,10 @@ public class TrigUnits {
             radians = true;
         }
     }
+    /**
+     * @param mode --> takes a mode and toggles the corresponding field.
+     * Selection alternative to cycling through trig units.
+     */
    public static void switchUnitsMode(String mode) {
         if (mode.toLowerCase().equals("radians")) {
             radians = true;
