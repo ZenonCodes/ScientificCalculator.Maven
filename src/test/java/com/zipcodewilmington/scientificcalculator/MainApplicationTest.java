3f2be0011851e3e60ab16b8e7cb9541415597d5f
Dan <dan@Zipcoders-MacBook-Pro.local>
2022-02-27 19:35:10 -0500

    updated some tests including display

--- src/test/java/com/zipcodewilmington/scientificcalculator/MainApplicationTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/MainApplicationTest.java
@@ -10,7 +10,7 @@ class MainApplicationTest {
         Logarithms test3 = new Logarithms();
         Factorial test4 = new Factorial();
         Console test5 = new Console();
-        Display test6 = new Display();
+//        Display test6 = new Display();
 //Get state test
 //        @Test
 //        void getStateORShowDisplay() {
@@ -194,100 +194,5 @@ class MainApplicationTest {
         Double getDoubleResult = test5.getDoubleInput("Test");
         Assertions.assertEquals(expectedIntegerInput,getDoubleResult);
     }
-//        @Test
-//        public void testSubtraction () {
-//            int subtractionTestA = 30;
-//            int subtractionTestB = 20;
-//            int expectedSubtraction = 10;
-//
-////            int subtractionResult = newCalculatorTest.subtract(subtractionTestA, subtractionTestB);
-//            Assertions.assertEquals(expectedSubtraction, subtractionResult);
-//        }
-//        @Test
-//        public void testMultiply () {
-//            int multiplyTestA = 10;
-//            int multiplyTestB = 20;
-//            int expectedMultiply = 200;
-//
-//            int multiplyResult = newCalculatorTest.multiply(multiplyTestA, multiplyTestB);
-//            Assertions.AssertEquals(expectedMultiply, multiplyResult);
-//        }
-//        @Test
-//        public void testDivision () {
-//            int divisionTestA = 20;
-//            int divisionTestB = 10;
-//            int expectedDivision = 2;
-//
-//            int divisionResult = newCalculatorTest.division(divisionTestA, divisionTestB);
-//            Assert.AssertEquals(expectedDivision, divisionResult);
-//        }
-//        @Test
-//        public void testSquare () {
-//            int squaredNumberA = 4;
-//            int squaredPowerB = 2;
-//            int expectedSquareExpected = 16;
-//
-//            int squaredResult = newCalculatorTest.getSquare(squaredNumberA, squaredPowerB);
-//            Assert.AssertEquals(expectedSquareExpected, squaredResult);
-//        }
-//        @Test
-//        public void testSquareRoot () {
-//            int squareRootTestA = 4;
-//            int squareRootTestB = 2;
-//            int expectedSquareRoot = 2;
-//
-//            int squareRootResult = newCalculatorTest.getSquareRoot(squareRootTestA, squareRootTestB);
-//            Assert.AssertEquals(expectedSquareRoot, squareRootResult);
-//        }
-//        @Test
-//        public void testXtotheY () {
-//            int xForTheY = 4;
-//            int yForTheX = 2;
-//            int expectedVariableProductResult = 16;
-//
-//            int variableProductResult = newCalculatorTest.getExponentiation(xForTheY, yForTheX);
-//            Assert.AssertEquals(expectedVariableProductResult, variableProductResult);
-//        }
-//        @Test
-//        public void testInverseX () {
-//            int inverseXValue = 5;
-//            double expectedInverseResult = 1/5;
-//
-//            int inverseResult = newCalculatorTest.getInverseOfNumber(inverseXValue);
-//            Assert.AssertEquals(expectedInverseResult, inverseResult);
-//        }
-//        @Test
-//        public void testSineX () {
-//            double testSineValueRadians = 0.19 * Math.PI;
-//            double expectedSineX = 83/144;
-//
-//            double sineResult = newCalculatorTest.sine(sineValueRadians);
-//            Assert.AssertEquals(expectedSineX, sineResult);
-//        }
-//        @Test
-//        public void testCosineX () {
-//            double testCosineValueRadians = 0.19 * Math.PI;
-//            double expectedCosineX = Math.sqrt(3) / 2;
-//
-//            double cosineResult = newCalculatorTest.cosine(cosineValueRadians);
-//            Assert.AssertEquals(expectedCosineX, cosineResult);
-//        }
-//        @Test
-//        public void testTangentX () {
-//            double testTangentValueRadians = Math.PI / 6;
-//            double expectedTangentX = Math.sqrt(3) / 3;
-//
-//            double tangentResult = newCalculatorTest.tangent(testTangentValueRadians);
-//            Assert.AssertEquals(expectedTangentX, tangentResult);
-//        }
-//        @Test
-//        public void testFactorial() {
-//            double testFactorialValue = 5;
-//            double expectedFactorialResult = 120;
-//
-//            double factorialResult = newCalculatorTest.factorial(testFactorialValue);
-//            Assert.AssertEquals(expectedFactorialResult, factorialResult);
-//
-//        }
     }
 
